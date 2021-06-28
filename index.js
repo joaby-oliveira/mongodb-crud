938fc8f8ae53263bb1da68f74e4ecca6984a59b1
Joaby <joabyoliveira2004@gmail.com>
2021-06-28 16:20:28 -0300

    create comments in code

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 const mongoose = require('mongoose')
 const articleModel = require('./article')
 
+// Create connection with database
 mongoose.connect('mongodb://localhost:27017/aprendendoMongo', { useNewUrlParser: true, useUnifiedTopology: true })
 
+// Create article schema
 const Article = mongoose.model('Article', articleModel)
 
+// Making a query
 Article.find({ 'resume.author': 'Joaby' }).then(articles => {
   console.log(articles)
 }).catch(err => {
   console.log(err)
 })
 
+// Delete data
 Article.findByIdAndDelete('60d9f8f837a68624c8978489').then(() => {
   console.log('Artigo excluído com sucesso')
 }).catch(err => {
   console.log(err)
 })
 
+// Register data
 const artigo = new Article({
   title: 'Aprendendo C# do ZERO',
   author: 'Joaby',
