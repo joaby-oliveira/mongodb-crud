6a2ea4509da44da4d5c32a1dff21300352efd64d
Joaby <joabyoliveira2004@gmail.com>
2021-06-28 13:03:31 -0300

    create data saving feature

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const mongoose = require('mongoose')
+const articleModel = require('./article')
 
 mongoose.connect('mongodb://localhost:27017/aprendendoMongo', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const Article = mongoose.model('Article', articleModel)
+
+const artigo = new Article({ title: 'Aprendendo Mongo com NodeJS', author: 'Joaby', body: 'Faça a conexão com o node e seja feliz (:' })
+
+artigo.save().then(() => {
+  console.log('Artigo salvo!')
+}).catch(err => {
+  console.log(err)
+})
